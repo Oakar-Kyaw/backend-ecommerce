8e42909d33dfc7f6544a99a22c1bb668afd7887d
Oakar-Kyaw <oakarkyaw7090@example.com>
2025-09-14 20:40:07 +0630

    fix(devop): add initial devops

--- libs/config/envConfig.ts
+++ libs/config/envConfig.ts
@@ -7,10 +7,10 @@ export const GlobalConfigModule = ConfigModule.forRoot({
 });
 
 export const envConfig =  () => ({
-    user_service_port: parseInt(process.env.USER_SERVER_PORT ?? '3000', 10),
+    user_service_port: parseInt(process.env.USER_SERVER_PORT ?? '5001', 10),
     user_service_url: process.env.USER_SERVER_URL ?? "http://localhost:4000/api/v1",
     user_service_tcp: parseInt(process.env.USER_SERVER_TCP ?? '5002', 10),
-    auth_service_port: parseInt(process.env.AUTH_SERVER_PORT ?? '5000', 10),
+    auth_service_port: parseInt(process.env.AUTH_SERVER_PORT ?? '5003', 10),
     auth_service_url: process.env.AUTH_SERVER_URL ?? "http://localhost:5000/api/v1",
     auth_service_tcp: parseInt(process.env.AUTH_SERVER_TCP ?? '5004', 10),
     JWTRefreshSecret: process.env.JWT_REFRESH_SECRET,
