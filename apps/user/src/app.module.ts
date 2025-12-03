8594cca28f7e269be6c3baacbd6793a3f952d85a
oakarkyaw <oakarkyaw7090@gmail.com>
2025-12-03 21:11:46 +0630

    feat(addlogs): add log in each server and fix error of no code and use relative import

--- apps/user/src/app.module.ts
+++ apps/user/src/app.module.ts
@@ -2,7 +2,7 @@
 import { MiddlewareConsumer, Module } from '@nestjs/common';
 import { UserModule } from './user.module';
 import { BrandModule } from './brand.module';
-import { RequestLoggerMiddleware } from 'libs/logs/logs';
+import { RequestLoggerMiddleware } from '../../../libs/logs/logs';
 
 @Module({
   imports: [
