64711e17e50a57f2d886a6544e53462dbd4370fd
oakarkyaw <oakarkyaw7090@gmail.com>
2025-12-03 21:22:04 +0630

    feat(addlogs): add log in each server and fix error of no code and use relative import

--- apps/auth/src/auth.module.ts
+++ apps/auth/src/auth.module.ts
@@ -6,7 +6,7 @@ import { envConfig } from 'libs/config/envConfig';
 import { AuthWorker } from './auth.worker';
 import { PublishMessageModule } from 'libs/queue/publish.module';
 import { AuthPrismaService } from '../prisma/auth.prisma.service';
-import { RequestLoggerMiddleware } from '../../../libs/logs/logs';
+import { RequestLoggerMiddleware } from '../../../libs/logs/logs-implementation';
 
 @Module({
   imports: [
--- apps/product/src/app.module.ts
+++ apps/product/src/app.module.ts
@@ -2,7 +2,7 @@
 import { MiddlewareConsumer, Module } from '@nestjs/common';
 import { ProductModule } from './product.module';
 import { CategoryModule } from './categories/category.module';
-import { RequestLoggerMiddleware } from '../../../libs/logs/logs';
+import { RequestLoggerMiddleware } from '../../../libs/logs/logs-implementation';
 
 @Module({
   imports: [ProductModule, CategoryModule],
