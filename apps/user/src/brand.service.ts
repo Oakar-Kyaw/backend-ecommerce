e889034eb415d5e7219dcc721b0b97cfb5d7f477
oakarkyaw <oakarkyaw7090@gmail.com>
2025-12-04 09:27:36 +0630

    feat(addlogs): only build server which made change

--- apps/user/src/brand.service.ts
+++ apps/user/src/brand.service.ts
@@ -94,7 +94,7 @@ export class BrandService {
       }
       where.createdAt = createdAt;
     }
-    
+
     const page = query?.page ? Number(query.page) : undefined;
     const pageSize = query?.pageSize ? Number(query.pageSize) : undefined;
     const order = query?.order === 'asc' ? 'asc' : 'desc';
@@ -183,7 +183,7 @@ export class BrandService {
   // ===== SOFT DELETE BRAND =====
   async softDelete(id: number) {
     const brand = await this.prisma.brand.findUnique({ where: { id } });
-    if (!brand) throw new NotFoundException(`Brand with ID ${id} not found`);
+    if (!brand) throw new NotFoundException(`Brand with ID ${id} not found.`);
 
     const deletedBrand = await this.prisma.brand.update({
       where: { id },
